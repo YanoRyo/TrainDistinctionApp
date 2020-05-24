c0096ab372e4e399dd2dcebf5cddd3935ffd96c9
YanoRyo <yanoryo7032@gmail.com>
2020-05-24 14:55:15 +0900

    Initial Commit

---
+++ TrainDistinctionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainDistinctionApp
+//
+//  Created by 矢野涼 on 2020-05-24.
+//  Copyright © 2020 Ryo Yano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
